fb4a463c4cb14413683158b375b3c22b929a70a6
godwu69 <godwu69@gmail.com>
2023-12-27 20:41:16 +0700

    update

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$int_var = 12345;
+$another_int = -12345 + 12345;
+$many = 2.222222;
+$few = $many + $many_2;
+
+
+$array_1[1000] = "This is an array for demo";
+$my_var = NULL;
+$string_1 = "This is a string in php, do dai cua string nay la tuy y";
+
+if(TRUE){
+    print("This is a message");
+    echo "message";
+}
+else{
+
+}
+?> 
+
+</body>
+</html>
